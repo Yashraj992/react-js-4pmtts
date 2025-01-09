1832eb51ed73b801a38175d559ea622c5c77848a
Yashraj992 <yashrajsinhpm@gmail.com>
2025-01-09 15:15:23 +0530

    09-01-2025

--- Module 7/emp-app/src/main.jsx
+++ Module 7/emp-app/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import Appp from './Appp'
+// import Appp from './Appp'
+import Swipe from './Swipe'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Appp />
+    {/* <Appp /> */}
+    <Swipe />
   </StrictMode>,  
 )
